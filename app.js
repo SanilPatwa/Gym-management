934821fe96eb60236a1b624f6dac80f404ab5824
Sanil <patwasanil39@gmail.com>
2024-10-01 19:44:04 +0530

    new

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.render("./listings/home.ejs");
+  res.render("./users/login.ejs");
 });
 app.listen("8080", () => {
   console.log("app is listening");
