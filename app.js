96459c64fc8f99979b2d21c82dc98b3ede86cfa2
Sanil <patwasanil39@gmail.com>
2024-10-02 16:04:00 +0530

    new

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/home", (req, res) => {
+  res.render("./listings/home.ejs");
+});
+app.get("/home", (req, res) => {
   res.render("./users/login.ejs");
 });
 app.listen("8080", () => {
